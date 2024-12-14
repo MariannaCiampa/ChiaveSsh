7daa8ddd93cafeb1a2ebeeb268b39ddff3baa970
Marianna Ciampa <marianna.ciampa.mc@gmail.com>
2024-12-14 22:53:57 +0100

    first commit di python

---
+++ Testiamo.py
@@ -0,0 +1,10 @@
+print("avrei voluto testare la chiave ssh"
+      "ma davvero non so come fare"
+      "mi sa che aspetterò la fine del token :D")
+
+animale_preferito= [gatto]
+
+print ("qual è il tuo animale_preferito?")
+
+print("Leo sei la mia vita")
+
