285539439bc73e69e0c39180612e57aa8f1bb459
Marianna Ciampa <marianna.ciampa.mc@gmail.com>
2024-12-14 23:05:22 +0100

    second commit di python

--- Testiamo.py
+++ Testiamo.py
@@ -1,10 +1,12 @@
 print("avrei voluto testare la chiave ssh"
       "ma davvero non so come fare"
       "mi sa che aspetterò la fine del token :D")
-
-animale_preferito= [gatto]
-
 print ("qual è il tuo animale_preferito?")
 
 print("Leo sei la mia vita")
+
+def fai_la_pasta():
+    print ("metti l'acqua")
+    print ("fai bollire")
+    print ("mangia")
 
